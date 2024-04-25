0e81797ae14f675a94a247b35bcf6593582024c7
nicolas follain <n.follain@yahoo.fr>
2024-04-25 17:23:56 +0200

    props1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,5 @@
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard({ pokemon }) {
+  console.log(pokemon);
 
   return (
     <figure>
@@ -13,14 +13,5 @@ function PokemonCard() {
     </figure>
   );
 }
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
 export default PokemonCard;
